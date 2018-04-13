3f20b0b73ecd47dc70cd5be173185a445dd6315f
WebsoftProfession <admin@mac-mini-4.local>
2018-04-13 15:35:13 +0530

    Initial Commit

---
+++ WSSlideMenu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WSSlideMenu
+//
+//  Created by admin on 13/04/18.
+//  Copyright © 2018 WebsoftProfession. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WSSlideMenu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WSSlideMenu
+//
+//  Created by admin on 13/04/18.
+//  Copyright © 2018 WebsoftProfession. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
